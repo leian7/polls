cb410f5d4a6a461a0c408dc59d05d5a551f0ea63
leian7 <leian@osuosl.org>
2016-04-20 12:39:07 -0700

    First commit

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+#namespace url names-- when proj has many apps, django needs to be able to differentiate between url names because two or more apps may have a view that is named the same
+
+app_name = 'polls' 
+urlpatterns= [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
